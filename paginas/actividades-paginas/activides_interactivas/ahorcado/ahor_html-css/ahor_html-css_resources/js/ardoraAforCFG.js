a214a45dd6b75c2e5cf10d3ae987516be8594896
John Arias <75335688+jaap627@users.noreply.github.com>
2020-12-05 18:24:21 -0500

    Prueba

---
+++ paginas/actividades-paginas/activides_interactivas/ahorcado/ahor_html-css/ahor_html-css_resources/js/ardoraAforCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=7; attempts=0; attemptsMax=10;
+var score=0; scoreMax=70; scoreInc=10; scoreDec=0
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=2;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="MUY BIEN"; messageTime="Limite de tiempo excedido"; messageError=""; messageErrorG=""; messageAttempts="Limite de intentos excedido. Has Perdido."; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="YWhvcl9odG1sLWNzcw"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var words=["TkFWRUdBRE9S","Q1NT","SFRNTA","RkxPQVQ","SEVBRA","Qk9EWQ","RVRJUVVFVEE","","",""];imaW=["","","","","","","","","",""];queW=["Se usa para acceder a páginas web","Siglas en ingles de \"Hojas de estilo en cascada\"","Lenguaje de marcado","Propiedad que permite que un elemento \"flote\"","Cabecera de un documento HTML","Cuerpo o contenido de un documento HTML","Fragmentos de texto rodeados por corchetes angulares <> con las cuales se escribe código HTML","","",""];
+var auW=["","","","","","","","","",""];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ "; wordsG=[]; indexG=0; actMaxWidth=550; indexWord=0; totalWord=0;
+var imageW=[];questionW=[];audioW=[];profG=0; dirMedia="ahor_html-css_resources/media/"; textBNext="";
